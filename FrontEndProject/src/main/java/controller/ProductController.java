227935b4a577828033ebde75ca5e5c7c702c65d5
user <user@vinodh>
2018-10-07 13:46:35 +0530

    Updated

--- FrontEndProject/src/main/java/controller/ProductController.java
+++ FrontEndProject/src/main/java/controller/ProductController.java
@@ -1,12 +1,8 @@
 package controller;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import model.Product;
 import model.ProductDAO;
@@ -24,22 +20,6 @@ public class ProductController {
 	{
 		ProductDAO pd = new ProductDAO();
 		pd.insertProduct(p);
-		return "listproducts";
+		return "success";
 	}
-	
-	@RequestMapping(value="/products", method=RequestMethod.GET)
-	@ResponseBody
-	public List<Product> getProducts()
-	{
-		ProductDAO pd = new ProductDAO();
-		List<Product> lp = null;
-		try {
-			lp = (List<Product>)pd.getProduct();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return lp;
-	}
-	
 }
